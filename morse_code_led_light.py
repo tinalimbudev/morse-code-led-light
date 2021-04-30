c932eaa657104d8eebd726e6476d8c44b1592953
tinalimbu <tinalawati@gmail.com>
2021-04-30 12:17:08 +0100

    Improved functionality

--- morse_code_led_light.py
+++ morse_code_led_light.py
@@ -1,28 +1,41 @@
 import RPi.GPIO as GPIO
 import time
 
+from functools import partial
 
-LED_PIN_NUMBER = 11
+
+LED_PIN_NUM = 11
+DASH_NUM_OF_SECONDS = 2
+DOT_NUM_OF_SECONDS = 1
+OFF_NUM_OF_SECONDS = 1
 
 
 def set_up_gpio():
 	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(LED_PIN_NUMBER, GPIO.OUT)
-	GPIO.output(LED_PIN_NUMBER, GPIO.LOW)
-
-
-def display_morse_code():
-	# GPIO.output(ledPin, GPIO.HIGH)  # make ledPin output HIGH level to turn on led
-	# print ('led turned on >>>')     # print information on terminal
-	# time.sleep(1)                   # Wait for 1 second
-	# GPIO.output(ledPin, GPIO.LOW)   # make ledPin output LOW level to turn off led
-	# print ('led turned off <<<')
-	# time.sleep(1)                   # Wait for 1 second
-	pass
+	GPIO.setup(LED_PIN_NUM, GPIO.OUT)
+	GPIO.output(LED_PIN_NUM, GPIO.LOW)
 
 
 def tear_down_gpio():
 	GPIO.cleanup()
+
+
+def display_morse_code():
+	pass
+
+
+def light_on(num_of_seconds):
+	GPIO.output(LED_PIN_NUM, GPIO.HIGH)
+	time.sleep(num_of_seconds)
+
+
+def light_off():
+	GPIO.output(LED_PIN_NUM, GPIO.LOW)
+	time.sleep(OFF_NUM_OF_SECONDS)
+
+
+dash_on = partial(light_on, num_of_seconds=DASH_NUM_OF_SECONDS)
+got_on = partial(light_on, num_of_seconds=DOT_NUM_OF_SECONDS)
 
 
 if __name__ == '__main__':
