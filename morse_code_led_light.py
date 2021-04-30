0e4623230d75ec7ec43250e9704224afbd4f629d
tinalimbu <tinalawati@gmail.com>
2021-04-30 12:28:44 +0100

    Cleaned text

--- morse_code_led_light.py
+++ morse_code_led_light.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import string
 import time
 
 from functools import partial
@@ -7,7 +8,8 @@ from functools import partial
 LED_PIN_NUM = 11
 DASH_NUM_OF_SECONDS = 2
 DOT_NUM_OF_SECONDS = 1
-OFF_NUM_OF_SECONDS = 1
+PAUSE_NUM_OF_SECONDS = 1
+SPACE_NUM_OF_SECONDS = 2
 
 
 def set_up_gpio():
@@ -20,8 +22,12 @@ def tear_down_gpio():
 	GPIO.cleanup()
 
 
-def display_morse_code():
-	pass
+def display_morse_code(text):
+	cleaned_text = clean_text(text)
+
+
+def clean_text(text):
+	return text.translate(str.maketrans("", "", string.punctuation))
 
 
 def light_on(num_of_seconds):
@@ -29,19 +35,21 @@ def light_on(num_of_seconds):
 	time.sleep(num_of_seconds)
 
 
-def light_off():
+def light_off(num_of_seconds):
 	GPIO.output(LED_PIN_NUM, GPIO.LOW)
-	time.sleep(OFF_NUM_OF_SECONDS)
+	time.sleep(num_of_seconds)
 
 
-dash_on = partial(light_on, num_of_seconds=DASH_NUM_OF_SECONDS)
-dot_on = partial(light_on, num_of_seconds=DOT_NUM_OF_SECONDS)
+dash = partial(light_on, num_of_seconds=DASH_NUM_OF_SECONDS)
+dot = partial(light_on, num_of_seconds=DOT_NUM_OF_SECONDS)
+pause = partial(light_off, num_of_seconds=PAUSE_NUM_OF_SECONDS)
+space = partial(light_off, num_of_seconds=SPACE_NUM_OF_SECONDS)
 
 
 if __name__ == '__main__':
     set_up_gpio()
 
     try:
-        display_morse_code()
+        display_morse_code("")  # Replace with text.
     except KeyboardInterrupt:
         tear_down_gpio()
